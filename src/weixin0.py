c81eab403f495257be46799b345ea317016287e2
zhouqiw <931384927@qq.com>
2018-02-17 23:51:12 +0800

    add a projcet

---
+++ src/weixin0.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+"""
+@author: zhouqi
+@software: PyCharm
+@file: weixin0.py
+@time: 2018/2/17 下午11:38
+"""
+import itchat,time,re
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
